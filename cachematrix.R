974b20cb1fc788a0db72562c5be34aa91fde8ebd
Rene Josiah Quinto <josiah.quinto@gmail.com>
2019-01-21 00:23:33 +0800

    Modifies base code to solution.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,29 @@
 
 ## Write a short comment describing this function
 
+# The enclosure of inverse matrix cache
 makeCacheMatrix <- function(x = matrix()) {
-
+    mat_inv <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() x
+    setinv <- function(inv) mat_inv <<- inv
+    getinv <- function() mat_inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    mat_inv <- x$getinv()
+    if(!is.null(mat_inv))
+        return(mat_inv)
+    
+    mat_inv <- solve(x$get())
+    x$setinv(mat_inv)
+    mat_inv
 }
