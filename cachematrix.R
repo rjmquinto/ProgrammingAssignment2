3a61c75bd3573e4bbf7d5a2068c555ee07431728
Rene Josiah Quinto <josiah.quinto@gmail.com>
2019-01-21 00:44:15 +0800

    Fixes bug on passing '...' parameters.

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
 # The enclosure of inverse matrix cache
 makeCacheMatrix <- function(x = matrix()) {
     mat_inv <- NULL
@@ -17,15 +15,12 @@ makeCacheMatrix <- function(x = matrix()) {
     list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
     mat_inv <- x$getinv()
     if(!is.null(mat_inv))
         return(mat_inv)
     
-    mat_inv <- solve(x$get())
+    mat_inv <- solve(x$get(), ...)
     x$setinv(mat_inv)
     mat_inv
 }
